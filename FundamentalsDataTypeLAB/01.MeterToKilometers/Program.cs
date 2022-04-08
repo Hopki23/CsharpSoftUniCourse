b5b7e0e17c88efbb114042dfc773a36635fd67e0
Hopki <edis.ersenov23@abv.bg>
2022-04-08 12:50:35 +0300

    Added new projects.

---
+++ FundamentalsDataTypeLAB/01.MeterToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1.MeterToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal meters = decimal.Parse(Console.ReadLine());
+            decimal kilometers = meters / 1000;
+            Console.WriteLine($"{kilometers:F2}");
+        }
+    }
+}
---
+++ FundamentalsDataTypeLAB/09.CharsToString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            string third = Console.ReadLine();
+
+            Console.WriteLine($"{first}{second}{third}");
+        }
+    }
+}
