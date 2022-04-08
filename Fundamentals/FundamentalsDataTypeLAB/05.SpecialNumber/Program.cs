7a4161be73e9bddb4cf82b6d955679ffdb6e855e
Hopki <edis.ersenov23@abv.bg>
2022-04-08 13:00:21 +0300

    Fixing files

---
+++ Fundamentals/FundamentalsDataTypeLAB/05.SpecialNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SpecialNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                int sumDigits = 0;
+                int digit = i;
+
+                while (digit > 0)
+                {
+                    sumDigits += digit % 10;
+                    digit /= 10;
+                }
+                bool isSpecial = false;
+
+                if (sumDigits == 5 || sumDigits == 7 || sumDigits == 11)
+                {
+                    isSpecial = true;
+                }
+
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
---
+++ Fundamentals/FundamentalsDataTypeLAB/06.ReversedChars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReversedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            string third = Console.ReadLine();
+
+            Console.WriteLine($"{third} {second} {first}");
+        }
+    }
+}
