d60ebe6600b37cafc5bc787c129cf73a83ed5471
Hopki <edis.ersenov23@abv.bg>
2022-04-08 12:56:36 +0300

    Added DataTypes-exercise.

---
+++ FundamentalsDataTypesEXE/05.PrintPartOfASCII/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrintPartOfASCII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+                char currentSymbol =(char)i;
+                Console.Write(currentSymbol + " ");
+            }
+        }
+    }
+}
---
+++ FundamentalsDataTypesEXE/10.Pokemon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());        //N
+            int distance = int.Parse(Console.ReadLine());        // M
+            int exhaustionFactor = int.Parse(Console.ReadLine()); //Y 
+            int pokeCounter = 0;
+            int startingPower = pokePower;
+
+            while (pokePower >= distance)
+            {
+                pokePower -= distance;
+                pokeCounter++;
+
+                if (pokePower == startingPower * 0.5)
+                {
+                    if (exhaustionFactor == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        pokePower /= exhaustionFactor;
+                    }
+                }
+            }
+            Console.WriteLine(pokePower);
+            Console.WriteLine(pokeCounter);
+        }
+    }
+}
