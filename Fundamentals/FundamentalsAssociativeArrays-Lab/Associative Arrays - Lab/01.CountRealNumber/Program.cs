c9c1e8a78d8fbf03ec5fe9c0765968446501c59d
Hopki <edis.ersenov23@abv.bg>
2022-04-11 16:22:11 +0300

    Added AssociativeArrays - LAB

---
+++ Fundamentals/FundamentalsAssociativeArrays-Lab/Associative Arrays - Lab/01.CountRealNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountRealNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+            SortedDictionary<double, int> dictionary = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (dictionary.ContainsKey(number))
+                {
+                    dictionary[number]++;
+                }
+                else
+                {
+                    dictionary.Add(number, 1);
+                }
+            }
+
+            foreach (var number in dictionary)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
---
+++ Fundamentals/FundamentalsAssociativeArrays-Lab/Associative Arrays - Lab/02.OddOccurances/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddOccurances
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            foreach (var item in words)
+            {
+                string wordsInLower = item.ToLower();
+
+                if (dictionary.ContainsKey(wordsInLower))
+                {
+                    dictionary[wordsInLower]++;
+                }
+                else
+                {
+                    dictionary.Add(wordsInLower, 1);
+                }
+            }
+
+            foreach (var word in dictionary)
+            {
+                if (word.Value % 2 != 0)
+                {
+                    Console.Write(word.Key + " ");
+                }
+            }
+        }
+    }
+}
