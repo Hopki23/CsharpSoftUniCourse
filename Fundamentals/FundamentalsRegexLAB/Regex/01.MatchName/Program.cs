bbcb1706f42388497d7b0a1d64bc30add1a42da4
Hopki <edis.ersenov23@abv.bg>
2022-04-13 16:36:44 +0300

    Added Regex - LAB

---
+++ Fundamentals/FundamentalsRegexLAB/Regex/01.MatchName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach (Match match in matches)
+            {
+                Console.Write(match + " ");
+            }
+        }
+    }
+}
---
+++ Fundamentals/FundamentalsRegexLAB/Regex/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> phoneNumbers = new List<string>(); 
+            string pattern = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+            string input = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                phoneNumbers.Add(match.Value);
+            }
+            Console.WriteLine(string.Join(", ",phoneNumbers));
+        }
+    }
+}
