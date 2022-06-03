5d3c9683795ba7882c3661f779b23703e87c2007
Hopki <edis.ersenov23@abv.bg>
2022-06-03 13:12:04 +0300

    Added DefiningClasses solutions.

---
+++ Advanced/DefiningClassesLAB/01.Car/Car.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Tire[] tire;
+        private Engine engine;
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year)
+            : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption,
+            Engine engine, Tire[] tires)
+            :this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            Engine = engine;
+            Tires = tires;
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+        public Engine Engine  { get; set; }
+        public Tire[] Tires { get; set; }
+
+        public void Drive(double distance)
+        {
+            bool isEnough = fuelQuantity - (distance * fuelConsumption) >= 0;
+
+            if (isEnough)
+            {
+                fuelQuantity -= distance * fuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {Make}");
+            sb.AppendLine($"Model: {Model}");
+            sb.AppendLine($"Year: {Year}");
+            sb.AppendLine($"Fuel: {FuelConsumption:F2}");
+
+            return sb.ToString();
+        }
+    }
+}
