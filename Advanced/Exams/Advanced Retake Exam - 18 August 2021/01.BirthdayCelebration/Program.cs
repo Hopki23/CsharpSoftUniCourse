983c00d4c5b8efbec7111d1adc141afd1fe6abb7
Hopki <edis.ersenov23@abv.bg>
2022-06-13 17:23:26 +0300

    New exam solutions

---
+++ Advanced/Exams/Advanced Retake Exam - 18 August 2021/01.BirthdayCelebration/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BirthdayCelebration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> guestCapacity = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> plate = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int wastedFood = 0;
+
+            while (guestCapacity.Count > 0 && plate.Count > 0)
+            {
+                int currentGuest = guestCapacity.Peek();
+                int currentPlate = plate.Peek();
+
+                if (currentPlate - currentGuest >= 0)
+                {
+                    wastedFood += currentPlate - currentGuest;
+                    guestCapacity.Dequeue();
+                    plate.Pop();
+                }
+                else if (currentPlate - currentGuest < 0)
+                {
+                    while (true)
+                    {
+                        currentGuest -= currentPlate;
+                        if (currentGuest <= 0)
+                        {
+                            wastedFood +=Math.Abs(currentGuest);
+                            break;
+                        }
+                        plate.Pop();
+                       currentPlate = plate.Peek();
+                    }
+                    guestCapacity.Dequeue();
+                    plate.Pop();
+                }
+            }
+
+            if (guestCapacity.Count == 0)
+            {
+                Console.WriteLine($"Plates: {string.Join(" ", plate)}");
+            }
+            else if (plate.Count == 0)
+            {
+                Console.WriteLine($"Guests: {string.Join(" ", guestCapacity)}");
+            }
+
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+        }
+    }
+}
---
+++ Advanced/Exams/Advanced Retake Exam - 18 August 2021/02.BattleOfFiveArmies/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.BattleOfFiveArmies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int armor = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            char[][] jagged = new char[n][];
+            int armyRow = 0;
+            int armyCol = 0;
+            for (int i = 0; i < n; i++)
+            {
+                jagged[i] = Console.ReadLine().ToCharArray();
+            }
+            for (int row = 0; row < jagged.GetLength(0); row++)
+            {
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    if (jagged[row][col] == 'A')
+                    {
+                        armyRow = row;
+                        armyCol = col;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string[] arr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string direction = arr[0];
+                int row = int.Parse(arr[1]);
+                int col = int.Parse(arr[2]);
+
+                jagged[row][col] = 'O';
+                jagged[armyRow][armyCol] = '-';
+                armor--;
+
+                if (direction == "up" && isValid(n, jagged, armyRow - 1, armyCol))
+                {
+                    armyRow--;
+                }
+                else if (direction == "down" && isValid(n, jagged, armyRow + 1, armyCol))
+                {
+                    armyRow++;
+                }
+                else if (direction == "left" && isValid(n, jagged, armyRow, armyCol - 1))
+                {
+                    armyCol--;
+                }
+                else if (direction == "right" && isValid(n, jagged, armyRow, armyCol + 1))
+                {
+                    armyCol++;
+                }
+
+
+                if (jagged[armyRow][armyCol] == 'M')
+                {
+                    jagged[armyRow][armyCol] = '-';
+                    Console.WriteLine($"The army managed to free the Middle World! Armor left: {armor}");
+                    break;
+                }
+
+                if (armor <= 0)
+                {
+                    break;
+                }
+
+                if (jagged[armyRow][armyCol] == 'O')
+                {
+                    armor -= 2;
+                    if (armor <= 0)
+                    {
+                        break;
+                    }
+                    jagged[armyRow][armyCol] = 'A';
+                }
+                else
+                {
+                    jagged[armyRow][armyCol] = 'A';
+                }
+            }
+
+            if (armor <= 0)
+            {
+                jagged[armyRow][armyCol] = 'X';
+                Console.WriteLine($"The army was defeated at {armyRow};{armyCol}.");
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    Console.Write(jagged[row][col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public static bool isValid(int rows, char[][] jagged, int row, int col)
+        {
+            if (row >= 0 && row < rows && col < jagged[row].Length && col >= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
