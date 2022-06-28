8e38b617f7ee16dcc98debb684cb995d204ad501
Hopki <edis.ersenov23@abv.bg>
2022-06-28 15:09:33 +0300

    Inheritance exercise solutions.

---
+++ OOP/Inheritance/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public const double DefaultFuelConsumption = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public virtual void Drive(double kilometers)
+        {
+            if (Fuel - (kilometers * FuelConsumption) >= 0)
+            {
+                Fuel -= kilometers * FuelConsumption;
+            }
+        }
+    }
+}
---
+++ OOP/Inheritance/Person/Person.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public virtual int Age
+        {
+            get { return age; }
+
+            set
+            {
+                if (age < 0)
+                {
+                    throw new ArgumentException("Age can not be less than zero!");
+                }
+
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
